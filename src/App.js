be58a06a3ebfb8482d0181d3565af4966ab90637
Federico Gómez <federicogomez.ar@gmail.com>
2023-01-11 20:58:47 +0100

    Correción error con logo y css de aplicación base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <h1>Hola mundo!</h1>
+      <header className="App-header">
+        <h1>Hola mundo!</h1>
+      </header>
+      
     </div>
   );
 }
