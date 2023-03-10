982f6cb3c060d10a3f52b47cd8fcbcbbe46d57e8
Hector Perez <mstrhache@gmail.com>
2023-03-10 11:05:02 +0000

    nueva backend modificación de gráfico

--- src/sections/@dashboard/app/AppWebsiteVisits.js
+++ src/sections/@dashboard/app/AppWebsiteVisits.js
@@ -25,14 +25,14 @@ export default function AppWebsiteVisits({
 		plotOptions: { bar: { columnWidth: '16%' } },
 		fill: { type: chartData.map(i => i.fill) },
 		labels: chartLabels,
-		xaxis: { type: 'datetime' },
+		xaxis: { type: 'string' },
 		tooltip: {
 			shared: true,
 			intersect: false,
 			y: {
 				formatter: y => {
 					if (typeof y !== 'undefined') {
-						return `${y.toFixed(0)} visits`;
+						return `${y.toFixed(0)} routines`;
 					}
 					return y;
 				},
